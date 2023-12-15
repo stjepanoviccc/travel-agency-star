c5579db22a7b9cda15ee8a2641c0ad2b45e97bd8
stjepanoviccc <stjepanoviccc2021@gmail.com>
2023-12-15 03:56:09 +0100

    implemented internationalization with session locale resolver

--- src/main/java/com/sr182022/travelagencystar/bean/MyConfiguration.java
+++ src/main/java/com/sr182022/travelagencystar/bean/MyConfiguration.java
@@ -2,11 +2,21 @@ package com.sr182022.travelagencystar.bean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.HashMap;
+import java.util.Locale;
 
 @Configuration
 public class MyConfiguration {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+
     @Bean (name={"AppMemory"}, initMethod = "", destroyMethod = "")
     public AppMemory getAppMemory() {
         return new AppMemory();
--- src/main/java/com/sr182022/travelagencystar/controller/InternationalizationController.java
+++ src/main/java/com/sr182022/travelagencystar/controller/InternationalizationController.java
@@ -1,16 +1,14 @@
 package com.sr182022.travelagencystar.controller;
 
-import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 
-import java.io.IOException;
 import java.util.Locale;
 
 @Controller
@@ -20,29 +18,19 @@ public class InternationalizationController {
     public static final String LOCALIZATION_KEY = "localization";
 
     @Autowired
-    private ServletContext servletContext;
-
-    @Autowired
-    private MessageSource messageSource;
+    private LocaleResolver localeResolver;
 
     @GetMapping("changeLanguage")
-    public void index(@RequestParam(defaultValue="en") String lang, HttpSession session, HttpServletResponse response) throws IOException {
-        Locale localization = (Locale) session.getAttribute(LOCALIZATION_KEY);
-
-        if(localization.getLanguage().equals(lang)) {
-            response.sendRedirect("/");
-            return;
-        }
-        System.out.println(lang);
-
-        if(lang.equals("sr")) {
-            localization = Locale.forLanguageTag("sr");
+    public String changeLanguage(@RequestParam String lang, HttpServletRequest request, HttpServletResponse response) {
+        Locale newLocale = null;
+        if (lang.equals("sr")) {
+            newLocale = Locale.forLanguageTag("sr");
         } else if (lang.equals("en")) {
-            localization = Locale.ENGLISH;
+            newLocale = Locale.ENGLISH;
         }
 
-        session.setAttribute(InternationalizationController.LOCALIZATION_KEY, localization);
-        System.out.println(session.getAttribute(InternationalizationController.LOCALIZATION_KEY));
-        response.sendRedirect("/");
+        localeResolver.setLocale(request, response, newLocale);
+
+        return "redirect:/";
     }
 }
